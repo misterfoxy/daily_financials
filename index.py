4bc7289f5afdc4e5e5cbf36f58001ee053435bfa
misterfoxy <michaelscottfox1@gmail.com>
2019-06-10 05:26:10 +0000

    minor updates

--- index.py
+++ index.py
@@ -5,11 +5,6 @@ import requests
 from bs4 import BeautifulSoup
 import bs4
 
-from fastnumbers import isfloat 
-from fastnumbers import fast_float
-from multiprocessing.dummy import Pool as ThreadPool 
-
-
 import json
 
 response = requests.get("https://cloud.iexapis.com/stable/stock/SPY/quote?token=pk_66584401576d4f9d88c4e03e082f6375", timeout=240)
