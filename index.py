9a1de4c1fb87023e39d165dec713ef56ec5a6d0c
misterfoxy <michaelscottfox1@gmail.com>
2019-06-10 05:35:31 +0000

    added environment variables

--- index.py
+++ index.py
@@ -1,13 +1,18 @@
 import numpy as np
 import pandas as pd
 
+from dotenv import load_dotenv
+load_dotenv()
+import os
+api_key = os.getenv('APIKEY')
+
 import requests
 from bs4 import BeautifulSoup
 import bs4
 
 import json
 
-response = requests.get("https://cloud.iexapis.com/stable/stock/SPY/quote?token=pk_66584401576d4f9d88c4e03e082f6375", timeout=240)
+response = requests.get("https://cloud.iexapis.com/stable/stock/SPY/quote?token=" + str(api_key) + "", timeout=240)
 data = response.json()
 print('Close: ' + str(data['close']))
 print('One Day Change: '+ str(data['change']))
