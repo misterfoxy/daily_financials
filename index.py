674ba23123677474bdbdfa5a81ea3c9fbd62cb8f
misterfoxy <michaelscottfox1@gmail.com>
2019-06-11 02:09:16 +0000

    added mailing functionality to daily financials. crontab was edited on linux vm to only send after a day has closed

--- index.py
+++ index.py
@@ -3,8 +3,16 @@ import pandas as pd
 
 from dotenv import load_dotenv
 load_dotenv()
+
 import os
 api_key = os.getenv('APIKEY')
+user = os.getenv('GMAIL_USER')
+passkey = os.getenv('GMAIL_PASS')
+
+import smtplib 
+s = smtplib.SMTP('smtp.gmail.com',587)
+s.starttls()
+s.login(str(user),str(passkey))
 
 import requests
 from bs4 import BeautifulSoup
@@ -16,3 +24,11 @@ response = requests.get("https://cloud.iexapis.com/stable/stock/SPY/quote?token=
 data = response.json()
 print('Close: ' + str(data['close']))
 print('One Day Change: '+ str(data['change']))
+
+
+message = 'Closing price of $' + str(data['close']) + ' for the SPY index today, a change of ' + str(data['change']) + '!'
+
+s.sendmail(str(user), 'michaelscottfox1@gmail.com', message)
+
+s.quit()
+
